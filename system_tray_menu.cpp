e91b0473a71f5c2242af4bb068097a90e2b355d8
lenky gao <gaoqunkai@ubuntukylin.com>
2013-08-16 15:39:53 +0800

    重启菜单项

--- system_tray_menu.cpp
+++ system_tray_menu.cpp
@@ -1,6 +1,8 @@
 #include <QDebug>
 #include <QDesktopServices>
 #include <QUrl>
+
+#include "fcitx-utils/utils.h"
 #include "system_tray_menu.h"
 
 SystemTrayMenu::SystemTrayMenu()
@@ -18,9 +20,20 @@ void SystemTrayMenu::init()
 {
     this->addAction(QIcon::fromTheme("help-contents"),
         tr("Online &Help!"), this, SLOT(clickOnlineHelp()));
+
+    this->addAction(QIcon::fromTheme("system-restart"),
+        tr("Restart"), this, SLOT(clickSystemRestart()));
 }
 
 void SystemTrayMenu::clickOnlineHelp()
 {
     QDesktopServices::openUrl(QUrl("http://fcitx-im.org/"));
 }
+
+void SystemTrayMenu::clickSystemRestart()
+{
+    fcitx_utils_launch_tool("fcitx-qimpanel", "-r");
+    fcitx_utils_launch_restart();
+}
+
+
--- system_tray_menu.h
+++ system_tray_menu.h
@@ -16,6 +16,7 @@ public:
 
 private slots:
     void clickOnlineHelp();
+    void clickSystemRestart();
 
 };
 

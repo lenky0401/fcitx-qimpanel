482788dc30e7fbd12254ebfe1417fa94162fc750
FJKong <kroodywar3@gmail.com>
2013-10-12 23:04:09 +0800

    add skin/skinsogou.cpp skin/skinsogou.h

---
+++ skin/skinsogou.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013 National University of Defense Technology(NUDT) & Kylin Ltd.
+ *
+ * Authors:
+ *  lenky gao    lenky0401@gmail.com/gaoqunkai@ubuntukylin.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SKIN_SOGOU_H__
+#define __SKIN_SOGOU_H__
+
+#include <QColor>
+#include "skinbase.h"
+
+class SkinSogou : public SkinBase
+{
+public:
+    SkinSogou();
+    virtual ~SkinSogou();
+    virtual bool loadSkin(const QString skinPath);
+};
+
+#endif // __SKIN_SOGOU_H__

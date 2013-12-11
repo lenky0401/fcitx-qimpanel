573729dfaa182fd7d8acc7d4908c7dec1c15ad6c
lmq3342xja <282553413@qq.com>
2013-12-11 14:28:15 +0800

    add sync configure

--- po/zh_CN.ts
+++ po/zh_CN.ts
@@ -4,7 +4,7 @@
 <context>
     <name>MainController</name>
     <message>
-        <location filename="../main_controller.cpp" line="194"/>
+        <location filename="../main_controller.cpp" line="204"/>
         <source>No input window</source>
         <translation>无输入窗口</translation>
     </message>
@@ -12,58 +12,101 @@
 <context>
     <name>SystemTrayMenu</name>
     <message>
-        <location filename="../system_tray_menu.cpp" line="42"/>
+        <location filename="../system_tray_menu.cpp" line="48"/>
         <source>Virtual Keyboard</source>
         <translation>虚拟键盘</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="43"/>
+        <location filename="../system_tray_menu.cpp" line="49"/>
         <source>Input Method</source>
         <translation>输入法</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="44"/>
+        <location filename="../system_tray_menu.cpp" line="51"/>
         <source>Skin</source>
         <translation>皮肤</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="85"/>
+        <location filename="../system_tray_menu.cpp" line="50"/>
+        <source>ConfigureSync</source>
+        <translation>配置同步</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="96"/>
         <source>No input window</source>
         <translation>无输入窗口</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="96"/>
-        <location filename="../system_tray_menu.cpp" line="225"/>
+        <location filename="../system_tray_menu.cpp" line="107"/>
+        <location filename="../system_tray_menu.cpp" line="375"/>
         <source>Online &amp;Help!</source>
         <translation>在线帮助</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="111"/>
-        <location filename="../system_tray_menu.cpp" line="228"/>
+        <location filename="../system_tray_menu.cpp" line="124"/>
+        <location filename="../system_tray_menu.cpp" line="378"/>
         <source>ConfigureIM</source>
         <translation>配置当前输入法</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="112"/>
-        <location filename="../system_tray_menu.cpp" line="234"/>
+        <location filename="../system_tray_menu.cpp" line="147"/>
+        <location filename="../system_tray_menu.cpp" line="394"/>
+        <source>ConfigureUp</source>
+        <translation>同步上传</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="148"/>
+        <location filename="../system_tray_menu.cpp" line="397"/>
+        <source>ConfigureDwon</source>
+        <translation>同步下载</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="315"/>
+        <location filename="../system_tray_menu.cpp" line="319"/>
+        <location filename="../system_tray_menu.cpp" line="343"/>
+        <location filename="../system_tray_menu.cpp" line="364"/>
+        <location filename="../system_tray_menu.cpp" line="369"/>
+        <source>Warning</source>
+        <translation>警告！</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="315"/>
+        <location filename="../system_tray_menu.cpp" line="364"/>
+        <source>[Ubuntu Kylin Sync] Synchronize directories have been deleted?!</source>
+        <translation>[Ubuntu Kylin Sync]该同步目录已经被删除？请自行创建。</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="319"/>
+        <location filename="../system_tray_menu.cpp" line="369"/>
+        <source>Please log in kuaipan!</source>
+        <translation>请登录金山快盘！</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="343"/>
+        <source>No configure can be synchronized!</source>
+        <translation>云端没有存储配置文件，无法同步到本地！</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="122"/>
+        <location filename="../system_tray_menu.cpp" line="384"/>
         <source>Configure</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="113"/>
-        <location filename="../system_tray_menu.cpp" line="231"/>
+        <location filename="../system_tray_menu.cpp" line="123"/>
+        <location filename="../system_tray_menu.cpp" line="381"/>
         <source>ConfigureIMPanel</source>
         <translation>配置面板和皮肤</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="116"/>
-        <location filename="../system_tray_menu.cpp" line="237"/>
+        <location filename="../system_tray_menu.cpp" line="128"/>
+        <location filename="../system_tray_menu.cpp" line="387"/>
         <source>Restart</source>
         <translation>重新启动</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="117"/>
-        <location filename="../system_tray_menu.cpp" line="241"/>
+        <location filename="../system_tray_menu.cpp" line="129"/>
+        <location filename="../system_tray_menu.cpp" line="391"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>

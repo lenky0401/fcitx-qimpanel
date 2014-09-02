7c17454ec58ecae3c7b90bda1f6b83ae1dba5679
高群凯 <lenky0401@gmail.com>
2014-09-02 15:21:12 +0800

    Update translation

--- po/zh_CN.ts
+++ po/zh_CN.ts
@@ -2,6 +2,184 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
+    <name>EditingSkinDialog</name>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="36"/>
+        <source>SkinInfo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="52"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="70"/>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="102"/>
+        <source>SkinFont</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="124"/>
+        <source>FontSize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="150"/>
+        <source>CandFontSize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="176"/>
+        <source>InputColor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="206"/>
+        <source>IndexColor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="236"/>
+        <source>FirstCandColor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="266"/>
+        <source>OtherColor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="298"/>
+        <source>SkinInputBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="336"/>
+        <source>BackImg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="362"/>
+        <source>TipsImg</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="388"/>
+        <source>MarginLeft</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="414"/>
+        <source>MarginRight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="440"/>
+        <source>MarginTop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="466"/>
+        <source>MarginBottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="492"/>
+        <source>InputStringPosX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="518"/>
+        <source>InputStringPosY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="544"/>
+        <source>OutputCandPosX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="570"/>
+        <source>OutputCandPosY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="596"/>
+        <source>BackArrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="622"/>
+        <source>BackArrowX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="648"/>
+        <source>BackArrowY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="674"/>
+        <source>ForwardArrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="700"/>
+        <source>ForwardArrowX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="726"/>
+        <source>ForwardArrowY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="752"/>
+        <source>AdjustWidth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="782"/>
+        <source>AdjustHeight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="829"/>
+        <source>&amp;Refresh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="857"/>
+        <source>&amp;Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.ui" line="876"/>
+        <source>&amp;Cannel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.cpp" line="276"/>
+        <source>tips</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/editingskindialog.cpp" line="276"/>
+        <source>The default configuration has been restored</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainController</name>
     <message>
         <location filename="../main_controller.cpp" line="225"/>
@@ -10,109 +188,211 @@
     </message>
 </context>
 <context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="26"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="39"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="56"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="68"/>
+        <source>Vertical List:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="84"/>
+        <source>Horizontal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="100"/>
+        <source>Vertical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="113"/>
+        <source>Skin Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="135"/>
+        <source>Skin Design</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="157"/>
+        <source>All Skin:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="173"/>
+        <source>Preview:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.ui" line="199"/>
+        <source>Description: Click to select and preview, double-click local to edit, save locally.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.cpp" line="37"/>
+        <source>Qimpanel Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.cpp" line="112"/>
+        <location filename="../config-tool/mainwindow.cpp" line="126"/>
+        <location filename="../config-tool/mainwindow.cpp" line="146"/>
+        <location filename="../config-tool/mainwindow.cpp" line="158"/>
+        <source>tips</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../config-tool/mainwindow.cpp" line="112"/>
+        <location filename="../config-tool/mainwindow.cpp" line="126"/>
+        <location filename="../config-tool/mainwindow.cpp" line="146"/>
+        <location filename="../config-tool/mainwindow.cpp" line="158"/>
+        <source>Sougo Skin does not support preview and edit!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>SystemTrayMenu</name>
     <message>
-        <location filename="../system_tray_menu.cpp" line="51"/>
+        <location filename="../system_tray_menu.cpp" line="57"/>
         <source>Virtual Keyboard</source>
         <translation>虚拟键盘</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="52"/>
+        <location filename="../system_tray_menu.cpp" line="58"/>
         <source>Input Method</source>
         <translation>输入法</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="54"/>
+        <location filename="../system_tray_menu.cpp" line="62"/>
         <source>Skin</source>
         <translation>皮肤</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="53"/>
+        <location filename="../system_tray_menu.cpp" line="60"/>
         <source>ConfigureSync</source>
         <translation>配置同步</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="99"/>
+        <location filename="../system_tray_menu.cpp" line="113"/>
         <source>No input window</source>
         <translation>无输入窗口</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="110"/>
-        <location filename="../system_tray_menu.cpp" line="378"/>
+        <location filename="../system_tray_menu.cpp" line="124"/>
+        <location filename="../system_tray_menu.cpp" line="422"/>
         <source>Online &amp;Help!</source>
         <translation>在线帮助</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="127"/>
-        <location filename="../system_tray_menu.cpp" line="381"/>
+        <location filename="../system_tray_menu.cpp" line="147"/>
+        <location filename="../system_tray_menu.cpp" line="425"/>
         <source>ConfigureIM</source>
         <translation>配置当前输入法</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="150"/>
-        <location filename="../system_tray_menu.cpp" line="402"/>
+        <location filename="../system_tray_menu.cpp" line="154"/>
+        <location filename="../system_tray_menu.cpp" line="454"/>
+        <source>Character Map</source>
+        <translation type="unfinished">字符映射表</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="155"/>
+        <location filename="../system_tray_menu.cpp" line="456"/>
+        <source>Keyboard Layout Chart</source>
+        <translation type="unfinished">键盘布局图</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="156"/>
+        <location filename="../system_tray_menu.cpp" line="476"/>
+        <source>Text Entry Settingsâ¦</source>
+        <translation type="unfinished">文本输入设置</translation>
+    </message>
+    <message>
+        <location filename="../system_tray_menu.cpp" line="180"/>
+        <location filename="../system_tray_menu.cpp" line="447"/>
         <source>ConfigureUp</source>
         <translation>同步上传</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="151"/>
-        <location filename="../system_tray_menu.cpp" line="405"/>
+        <location filename="../system_tray_menu.cpp" line="181"/>
+        <location filename="../system_tray_menu.cpp" line="450"/>
         <source>ConfigureDwon</source>
         <translation>同步下载</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="318"/>
-        <location filename="../system_tray_menu.cpp" line="322"/>
-        <location filename="../system_tray_menu.cpp" line="346"/>
-        <location filename="../system_tray_menu.cpp" line="367"/>
-        <location filename="../system_tray_menu.cpp" line="372"/>
-        <location filename="../system_tray_menu.cpp" line="388"/>
+        <location filename="../system_tray_menu.cpp" line="361"/>
+        <location filename="../system_tray_menu.cpp" line="365"/>
+        <location filename="../system_tray_menu.cpp" line="389"/>
+        <location filename="../system_tray_menu.cpp" line="410"/>
+        <location filename="../system_tray_menu.cpp" line="415"/>
+        <location filename="../system_tray_menu.cpp" line="432"/>
         <source>Warning</source>
         <translation>警告！</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="318"/>
-        <location filename="../system_tray_menu.cpp" line="367"/>
+        <location filename="../system_tray_menu.cpp" line="361"/>
+        <location filename="../system_tray_menu.cpp" line="410"/>
         <source>[Ubuntu Kylin Sync] Synchronize directories have been deleted?!</source>
         <translation>[Ubuntu Kylin Sync]该同步目录已经被删除？请自行创建。</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="322"/>
-        <location filename="../system_tray_menu.cpp" line="372"/>
+        <location filename="../system_tray_menu.cpp" line="365"/>
+        <location filename="../system_tray_menu.cpp" line="415"/>
         <source>Please log in kuaipan!</source>
         <translation>请登录金山快盘！</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="346"/>
+        <location filename="../system_tray_menu.cpp" line="389"/>
         <source>No configure can be synchronized!</source>
         <translation>云端没有存储配置文件，无法同步到本地！</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="388"/>
+        <location filename="../system_tray_menu.cpp" line="432"/>
         <source>Please install fcitx-qimpanel-configtool!</source>
         <translation>请安装配置工具：fcitx-qimpanel-configtool</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="125"/>
-        <location filename="../system_tray_menu.cpp" line="392"/>
+        <location filename="../system_tray_menu.cpp" line="145"/>
+        <location filename="../system_tray_menu.cpp" line="436"/>
         <source>Configure</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="126"/>
-        <location filename="../system_tray_menu.cpp" line="384"/>
+        <location filename="../system_tray_menu.cpp" line="146"/>
+        <location filename="../system_tray_menu.cpp" line="428"/>
         <source>ConfigureIMPanel</source>
         <translation>配置面板和皮肤</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="131"/>
-        <location filename="../system_tray_menu.cpp" line="395"/>
+        <location filename="../system_tray_menu.cpp" line="160"/>
+        <location filename="../system_tray_menu.cpp" line="439"/>
         <source>Restart</source>
         <translation>重新启动</translation>
     </message>
     <message>
-        <location filename="../system_tray_menu.cpp" line="132"/>
-        <location filename="../system_tray_menu.cpp" line="399"/>
+        <location filename="../system_tray_menu.cpp" line="161"/>
+        <location filename="../system_tray_menu.cpp" line="443"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
